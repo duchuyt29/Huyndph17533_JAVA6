2e9ad84b1923abd5368ed3ca4b83fa793330eb30
Nguyễn Đức Huy <huyndph17533@fpt.edu.vn>
2023-11-08 17:56:52 +0700

    Bai 3 hot Fix

--- Java6_Bai3/src/main/java/java6bai3/java6_bai3/beans/Student2.java
+++ Java6_Bai3/src/main/java/java6bai3/java6_bai3/beans/Student2.java
@@ -1,23 +1,31 @@
 package java6bai3.java6_bai3.beans;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Student2 {
-//        @NotBlank(message = "Không để trống email")
-//        String email;
-//        @NotBlank(message = "Không để trống fullname")
-//        String fullName;
-//        @NotNull(message = "Không để trống điểm")
-//        @PositiveOrZero(message = "Điểm phải lớn hơn hoặc bằng 0")
-//        @Max(value = 10,message = "Điểm phải nhỏ hơn hoặc bằng 10")
-//        Double marks;
-//        @NotNull(message = "Chọn giới tính")
-//        Boolean gender;
-//        @NotBlank(message = "Không để trống country")
-//        String country;
+
+        @NotBlank(message = "Không để trống email")
+        @Email(message = "Email không đúng định dạng")
+        String email;
+
+        @NotBlank(message = "Không để trống fullname")
+        String fullName;
+
+        @NotNull(message = "Không để trống điểm")
+        @PositiveOrZero(message = "Điểm phải lớn hơn hoặc bằng 0")
+        @Max(value = 10,message = "Điểm phải nhỏ hơn hoặc bằng 10")
+        Double marks;
+
+        @NotNull(message = "Chọn giới tính")
+        Boolean gender;
+
+        @NotBlank(message = "Không để trống country")
+        String country;
 }
